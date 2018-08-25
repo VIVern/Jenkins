60018460b099e49eb4833fd531b4189033522189
VIVern <d.fedioukovitch@godeltech.com>
2018-08-25 13:01:55 +0300

    syntax fix

--- app/HelloWorld.php
+++ app/HelloWorld.php
@@ -2,8 +2,16 @@
 
 namespace App;
 
+/**
+ * Class HelloWorld
+ * @package App
+ */
 class HelloWorld
 {
+    /**
+     * @param $name
+     * @return string
+     */
     public function sayHello($name)
     {
         return "Hello" . $name . "!";
