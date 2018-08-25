98abbeaff68d6a8c1f5a4bbe57a5dd8d08131586
VIVern <d.fedioukovitch@godeltech.com>
2018-08-25 13:32:16 +0300

    syntax fixed in test file

--- test/TestHelloWorld.php
+++ test/TestHelloWorld.php
@@ -5,8 +5,17 @@ namespace test;
 use App\HelloWorld;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class TestHelloWorld
+ * @package test
+ */
 class TestHelloWorld extends TestCase
 {
+    /**
+     * Test sayHello method.
+     *
+     * @return void
+     */
     public function testSayHello()
     {
         $helloWorld = new HelloWorld();
